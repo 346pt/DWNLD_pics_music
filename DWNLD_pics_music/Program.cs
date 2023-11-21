8ad2053ce81b00f160f5d2f058573f3e9d82b2d5
Антисептик <@DESKTOP-KTN6EI8>
2023-11-21 11:04:44 +0300

    Добавьте файлы проекта.

---
+++ DWNLD_pics_music/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        HttpClient client = new HttpClient();
+
+        Console.Write("Введите ссылку для скачивания изображения: ");
+        string webSiteName = Console.ReadLine();
+
+        HttpResponseMessage response = await client.GetAsync(webSiteName);
+        byte[] data = await response.Content.ReadAsByteArrayAsync();
+
+        Console.Write("Введите путь сохранения: ");
+        string path = Console.ReadLine();
+        await File.WriteAllBytesAsync(path, data);
+
+        Process.Start(new ProcessStartInfo { FileName = path, UseShellExecute = true });
+
+
+
+        HttpClient client1 = new HttpClient();
+
+        Console.Write("Введите ссылку для скачивания музыки: ");
+        string webSiteLink = Console.ReadLine();
+
+
+        HttpResponseMessage response1 = await client1.GetAsync(webSiteLink);
+        byte[] data1 = response1.Content.ReadAsByteArrayAsync().Result;
+
+        Console.Write("Введите путь сохранения: ");
+        string path_1 = Console.ReadLine();
+        File.WriteAllBytes(path_1, data1);
+
+        Process.Start(new ProcessStartInfo { FileName = path_1, UseShellExecute = true });
+    }
+}
